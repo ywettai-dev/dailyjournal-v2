8d25ce467520f33df7701a606d8d17b0255eb140
ywettai-dev <ywettai.dev@gmail.com>
2019-05-27 22:31:00 +0630

    Add partials folder under views folder

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const port = 3000;
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,18 +12,41 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+//ejs engine
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//express link for public - static folder
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3001);
+//Express server
+app.listen(process.env.PORT || port, () => console.log(`Daily Journal starts on ${port}`));
 
+//Home
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {homeInitialContent: homeStartingContent});
+});
+
+//About
+app.get('/about', (req, res) => {
+  res.render('about', {aboutInitialContent: aboutContent});
+});
+
+//Contact
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactInitialContent: contactContent});
+});
+
+//Compose
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  console.log(req.body.publishText);
 });
 
 
@@ -38,6 +62,4 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+
